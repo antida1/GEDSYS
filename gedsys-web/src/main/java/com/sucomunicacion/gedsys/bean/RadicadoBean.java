900d3f60261fc1c2c5d734ddf9f720ec34b70658
rober <rober@LAPTOP-T7TGRJFL>
2017-04-19 17:29:21 -0500

    Se agregan nuevas funcionalidades de Generacion de Radicado

--- gedsys-web/src/main/java/com/sucomunicacion/gedsys/bean/RadicadoBean.java
+++ gedsys-web/src/main/java/com/sucomunicacion/gedsys/bean/RadicadoBean.java
@@ -20,9 +20,11 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.servlet.ServletContext;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
@@ -31,58 +33,63 @@ import org.primefaces.model.StreamedContent;
  * @author rober
  */
 @ManagedBean
+@RequestScoped
 public class RadicadoBean extends BaseBean implements Serializable {
+
+    private String fileName;
+    private StreamedContent radicadoImg;
 
     @PostConstruct
     public void init() {
 
     }
 
-    private StreamedContent radicadoImg;
-
     public void setRadicadoImg(StreamedContent radicadoImg) {
         this.radicadoImg = radicadoImg;
     }
 
     public StreamedContent getRadicadoImg() {
-        EntityManagerFactory emf = JpaUtils.getEntityManagerFactory(configFilePath);
-        EntityManager em = emf.createEntityManager();
-        ConsecutivoJpaController cJpa;
         try {
-            em.getTransaction().begin();
+            EntityManagerFactory emf = JpaUtils.getEntityManagerFactory(configFilePath);
+            EntityManager em = emf.createEntityManager();
+            ConsecutivoJpaController cJpa;
 
+            em.getTransaction().begin();
             cJpa = new ConsecutivoJpaController(emf);
             Consecutivo consec = cJpa.findConsecutivoByTipoConsecutivo("Externo");
-
             Integer intConsec = Integer.parseInt(consec.getConsecutivo());
             intConsec++;
-
             consec.setConsecutivo(intConsec.toString());
-            
             em.merge(consec);
             em.flush();
-           
+
             RadicadoImage ri = new RadicadoImage();
             SimpleDateFormat sdfDateRadicado = new SimpleDateFormat("yyyyMMdd");
             Date hoy = new Date();
             String strHoy = sdfDateRadicado.format(hoy);
 
-            String name = consec.getPrefijo() + strHoy + consec.getConsecutivo() + consec.getPrefijo();
-            String fileName = ri.Generar(name, configFilePath);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();
+            String path = servletContext.getRealPath("/resources/images");
 
-            //TODO: Cambiar la ruta para que tome la ruta de la configuración
-            File file = new File("F:" + File.separatorChar + fileName);
+            String name = consec.getPrefijo() + strHoy + consec.getConsecutivo() + consec.getSufijo();
+            fileName = ri.Generar(name, path);
+
+            em.getTransaction().commit();
+
+            File file = new File("F:" + File.separatorChar + this.fileName);
             if (file.canRead()) {
                 FileInputStream fi = new FileInputStream(file);
                 this.radicadoImg = new DefaultStreamedContent(fi, null, file.getName());
             }
-            em.getTransaction().commit();
-            
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage( FacesMessage.SEVERITY_FATAL  ,"Error!", e.getMessage()));
+        } catch (FileNotFoundException e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error!", e.getMessage()));
             Logger.getLogger(RadicadoBean.class.getName()).log(Level.SEVERE, e.getMessage());
-        } 
+        }
         return radicadoImg;
+    }
+
+    public void generar() {
+
     }
 }
