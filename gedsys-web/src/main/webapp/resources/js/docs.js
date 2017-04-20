5413610eed96f83c5d08282521731e1bb1189b2e
Alejandro Chaves <alejandrochvs@hotmail.com>
2017-04-20 11:08:39 -0500

    Tooltips add

--- gedsys-web/src/main/webapp/resources/js/docs.js
+++ gedsys-web/src/main/webapp/resources/js/docs.js
@@ -1,20 +1,23 @@
 $(function () {
-    function appendDoc(title, based, date, content,toAppend) {
+    function appendDoc(title, based, date, content, toAppend) {
         var date = date || "4/19/2017";
         var title = title || "Titulo"
         var content = content || "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Quisquam beatae, totam consectetur, ad, saepe voluptas eligendi ducimus, praesentium maxime dolore ab quod fugiat facilis debitis quis numquam inventore.Adipisci, saepe.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Quisquam beatae, totam consectetur, ad, saepe voluptas eligendi ducimus, praesentium maxime dolore ab quod fugiat facilis debitis quis numquam inventore.Adipisci, saepe.";
         var based = based || "#B201719042B";
         var toAppend = toAppend || $('#menuTab0');
-        var doc = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3 doc'><div class='col-xs-12 paper'><div class='col-xs-6 title'><h4>" + title + "</h4><h6>" + date + "</h6></div><div class='col-xs-6 based text-right'><h5>" + based + "</h5></div><div class='col-xs-10 col-xs-offset-1 content text-justify'><p>" + content + "</p></div><div class='btn-group btn-group-justified col-xs-12'><a href='#'class='btn btn-primary'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span></a><a href='#' class='btn btn-primary'><span class='glyphicon glyphicon-share-alt' aria-hidden='true'></span></a><a href='#' class='btn btn-primary'><span class='glyphicon glyphicon-eye-open' aria-hidden='true'></span></a><a href='#' class='btn btn-primary'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></a></div></div></div>";
+        var doc = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3 doc'><div class='col-xs-12 paper'><div class='col-xs-6 title'><h4>" + title + "</h4><h6>" + date + "</h6></div><div class='col-xs-6 based text-right'><h5>" + based + "</h5></div><div class='col-xs-10 col-xs-offset-1 content text-justify'><p>" + content + "</p></div><div class='btn-group btn-group-justified col-xs-12'><a data-toggle='tooltip' data-placement='bottom' title='Editar' href='#'class='btn btn-primary'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span></a><a data-toggle='tooltip' data-placement='bottom' title='Reenviar' href='#' class='btn btn-primary'><span class='glyphicon glyphicon-share-alt' aria-hidden='true'></span></a><a data-toggle='tooltip' data-placement='bottom' title='Visualizar' href='#' class='btn btn-primary'><span class='glyphicon glyphicon-eye-open' aria-hidden='true'></span></a><a data-toggle='tooltip' data-placement='bottom' title='Borrar' href='#' class='btn btn-primary'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></a></div></div></div>";
         toAppend.children().append(doc);
+        $('[data-toggle="tooltip"]').tooltip({
+            container: 'body'
+        });
     }
     for (var i = 0; i < 20; i++) {
-        appendDoc(undefined,undefined,undefined,undefined,undefined);
+        appendDoc(undefined, undefined, undefined, undefined, undefined);
     }
     for (var i = 0; i < 10; i++) {
-        appendDoc(undefined,undefined,undefined,undefined,$('#menuTab1'));
+        appendDoc(undefined, undefined, undefined, undefined, $('#menuTab1'));
     }
     for (var i = 0; i < 3; i++) {
-        appendDoc(undefined,undefined,undefined,undefined,$('#menuTab2'));
+        appendDoc(undefined, undefined, undefined, undefined, $('#menuTab2'));
     }
 })
