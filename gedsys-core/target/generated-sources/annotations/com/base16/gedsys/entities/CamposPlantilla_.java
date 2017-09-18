844b8dc2489805f44a712b61ef11c28315e60421
alejandrochvs <alejandrochvs@hotmail.com>
2017-09-18 16:07:33 -0500

    Color change

---
+++ gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/CamposPlantilla_.java
@@ -0,0 +1,22 @@
+package com.base16.gedsys.entities;
+
+import com.base16.gedsys.entities.Modulo;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-09-18T15:43:39")
+@StaticMetamodel(CamposPlantilla.class)
+public class CamposPlantilla_ { 
+
+    public static volatile SingularAttribute<CamposPlantilla, Integer> creadoPor;
+    public static volatile SingularAttribute<CamposPlantilla, Boolean> estado;
+    public static volatile SingularAttribute<CamposPlantilla, Date> fechaModificacion;
+    public static volatile SingularAttribute<CamposPlantilla, Integer> modificadoPor;
+    public static volatile SingularAttribute<CamposPlantilla, Date> fechaCreacion;
+    public static volatile SingularAttribute<CamposPlantilla, Integer> id;
+    public static volatile SingularAttribute<CamposPlantilla, String> nombre;
+    public static volatile SingularAttribute<CamposPlantilla, Modulo> modulo;
+
+}
