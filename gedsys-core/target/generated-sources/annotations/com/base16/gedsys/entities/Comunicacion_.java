cee6ef273206c1c9d0a572d86748615fceb79684
Lina.David <programacion01@programacion01.sucomunicacion.local>
2018-01-23 16:45:22 -0500

    ajustes

--- gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Comunicacion_.java
+++ gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Comunicacion_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-20T00:16:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-22T12:37:31")
 @StaticMetamodel(Comunicacion.class)
 public class Comunicacion_ { 
 
--- gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Grupo_.java
+++ gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Grupo_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-20T00:16:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-22T12:37:31")
 @StaticMetamodel(Grupo.class)
 public class Grupo_ { 
 
