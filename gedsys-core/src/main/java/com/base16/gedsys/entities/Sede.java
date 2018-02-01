9ce87f279b55d31266b136c0916329678707a914
Lina.David <programacion01@programacion01.sucomunicacion.local>
2018-02-01 15:00:31 -0500

    config

--- gedsys-core/src/main/java/com/base16/gedsys/entities/Sede.java
+++ gedsys-core/src/main/java/com/base16/gedsys/entities/Sede.java
@@ -151,7 +151,7 @@ public class Sede implements Serializable {
         Sede other = (Sede) object;
         if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
-        }
+    }
         return true;
     }
 
