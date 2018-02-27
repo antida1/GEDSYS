ad0627856f2a8cbf6b53ff6c9c83cc84f6910f2c
Lina.David <programacion01@programacion01.sucomunicacion.local>
2018-02-27 10:35:08 -0500

    Revert 88e8147aafccd36c7aee3c266a52debbc6be51e3

--- gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Configuracion_.java
+++ gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Configuracion_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-27T10:29:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-02-27T09:08:20")
 @StaticMetamodel(Configuracion.class)
 public class Configuracion_ { 
 
