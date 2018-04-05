300a056ea0a9aadc250d53711345500fc704ec04
Lina.David <programacion01@programacion01.sucomunicacion.local>
2018-04-05 10:13:29 -0500

    se bajan los cambios

--- gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Comunicacion_.java
+++ gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Comunicacion_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T16:17:06")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-04T14:32:34")
 @StaticMetamodel(Comunicacion.class)
 public class Comunicacion_ { 
 
--- gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Usuario_.java
+++ gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Usuario_.java
@@ -59,7 +59,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T16:17:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-04T14:32:34")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
