1785fc76f223d46b5745162dcf55ef7435d32256
Lina.David <programacion01@programacion01.sucomunicacion.local>
2018-04-09 16:48:58 -0500

    Se organiza el modulo de búsqueda de consecutivos y ase ordena por fecha el modulo de gestión documental

--- gedsys-core/src/main/java/com/base16/gedsys/model/ConsecutivosUsuarioJpaController.java
+++ gedsys-core/src/main/java/com/base16/gedsys/model/ConsecutivosUsuarioJpaController.java
@@ -5,7 +5,9 @@
  */
 package com.base16.gedsys.model;
 
+import com.base16.gedsys.entities.Consecutivo;
 import com.base16.gedsys.entities.ConsecutivosUsuario;
+import com.base16.gedsys.entities.Documento_;
 import java.io.Serializable;
 import javax.persistence.Query;
 import javax.persistence.EntityNotFoundException;
@@ -13,9 +15,17 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import com.base16.gedsys.entities.Usuario;
 import com.base16.gedsys.model.exceptions.NonexistentEntityException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
 
 /**
  *
@@ -150,6 +160,63 @@ public class ConsecutivosUsuarioJpaController implements Serializable {
         } finally {
             em.close();
         }
+    }
+    
+     public List<ConsecutivosUsuario> findConsecutivos(Usuario creadoPor, String consecutivo, Date starDate, Date endDate, String tipo) {
+        return findConsecutivos(creadoPor, consecutivo, starDate, endDate, tipo, true, -1, -1);
+    }
+
+    public List<ConsecutivosUsuario> findConsecutivos(Usuario creadoPor, String consecutivo, Date starDate, Date endDate, String tipo, int maxResults, int firstResult) {
+        return findConsecutivos(creadoPor, consecutivo, starDate, endDate, tipo, false, maxResults, firstResult);
+    }
+
+    private List<ConsecutivosUsuario> findConsecutivos(Usuario creadoPor, String consecutivo, Date starDate, Date endDate, String tipo, boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        List<ConsecutivosUsuario> consecutivos = null;
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery cq = cb.createQuery(ConsecutivosUsuario.class);
+            
+            Root doc = cq.from(ConsecutivosUsuario.class);
+            List<Predicate> predicates = new ArrayList<Predicate>();
+
+            if(creadoPor != null){
+                Expression<Usuario> eCreadoPor = doc.get("creadoPor");
+                predicates.add(cb.equal(eCreadoPor, creadoPor));
+            }
+            
+            if (starDate != null && endDate != null) {
+                predicates.add(cb.and(cb.between(doc.get("fechaCreacion"), starDate, endDate)));
+            }
+            
+            if (tipo != null && !tipo.isEmpty()) {
+                String pTipo = "%" + tipo + "%";
+                Expression<String> etipo = doc.get("tipo");
+                predicates.add(cb.and(cb.like(etipo, pTipo)));
+            }
+            
+            if (consecutivo != null && !consecutivo.isEmpty()) {
+                String pConsecutivo = "%" + consecutivo + "%";
+                Expression<String> eRadicado = doc.get("consecutivo");
+                predicates.add(cb.and(cb.like(eRadicado, pConsecutivo)));
+            }
+            
+            cq.select(doc).where(predicates.toArray(new Predicate[]{}));
+        
+            TypedQuery<ConsecutivosUsuario> q = em.createQuery(cq);
+            
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            consecutivos = q.getResultList();
+
+        } catch (Exception e) {
+            Logger.getLogger(DocumentoJpaController.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            em.close();
+        }
+        return consecutivos;
     }
 
     public int getConsecutivosUsuarioCount() {
