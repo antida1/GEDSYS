3d57670d248a86bac4bce2932eebda2e510a6bc4
Lina.David <programacion01@programacion01.sucomunicacion.local>
2018-04-19 15:57:56 -0500

    Enviados actualización VER

--- gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Usuario_.java
+++ gedsys-core/target/generated-sources/annotations/com/base16/gedsys/entities/Usuario_.java
@@ -59,7 +59,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-19T10:25:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-19T14:41:12")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
